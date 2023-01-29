c3fa4d6fb60541d5483d740dac866a4f21a00cdd
sudh-202 <87563365+sudh-202@users.noreply.github.com>
2023-01-29 07:01:28 +0530

    new branch

--- src/content.js
+++ src/content.js
@@ -1,36 +1,36 @@
 const productData = [
-    {
-      id: 1,
-      name: "MASSDROP X SENNHEISER HD 6XX HEADPHONES",
-      image:
-        "https://massdrop-s3.imgix.net/product-images/massdrop-sennheiser-hd6xx/FP/cJolRuxYSgqdFtmFawp8_yJ6M80CfTZGYMcNKc5of_0cYrqmTrQFOf4cbRVO6t_H6xx%20Reshoot32572.png?auto=format&fm=jpg&fit=fill&w=300&h=300&bg=f0f0f0&fill=solid&fill-color=f0f0f0&dpr=1&q=70",
-      productType: "openBackHeadphones",
-      price: 220,
-      rating: 5,
-      timeLeft: 27,
-      totalSales: 7479 
-    },
-    {
-      id: 2,
-      name: "DROP + SENNHEISER PC38X GAMING HEADSET",
-      image:
-        "https://massdrop-s3.imgix.net/product-images/drop-sennheiser-pc38x-gaming-headset/FP/CeH9BYVoTniYOsScfzZ0_PC.png?auto=format&fm=jpg&fit=fill&w=300&h=300&bg=f0f0f0&fill=solid&fill-color=f0f0f0&dpr=1&q=70",
-      productType: "openBackHeadphones",
-      price: 169,
-      rating: 5,
-      timeLeft: 24,
-      totalSales: 6898
-    },
-    {
-      id: 3,
-      name: "MASSDROP X SENNHEISER HD 58X JUBILEE HEADPHONES",
-      image:
-        "https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/jbCgVcPdRXZC1YRI2oZ3_3qKeFvWGRbQ3wreRhIVc_58x_clear.png?auto=format&fm=jpg&fit=fill&w=300&h=300&bg=f0f0f0&fill=solid&fill-color=f0f0f0&dpr=1&q=70",
-      productType: "openBackHeadphones",
-      price: 170,
-      rating: 4,
-      timeLeft: 21,
-      totalSales: 6347 
-    } 
-  ];
+  {
+    id: 1,
+    name: "Audi Suv",
+    image:
+      "https://pluspng.com/img-png/car-png-png-file-name-car-png-hd-dimension-1195x752-image-type-png-posted-on-may-11th-2017-category-cars-transportation-tags-car-1195.png",
+    productType: "SUV Cars",
+    price: 220000,
+    rating: 5,
+    timeLeft: 27,
+    totalSales: 479,
+  },
+  {
+    id: 2,
+    name: "Audi 8",
+    image:
+      "https://purepng.com/public/uploads/large/purepng.com-yellow-audi-caraudicars-961524670899johme.png",
+    productType: "Super Cars",
+    price: 320000,
+    rating: 5,
+    timeLeft: 24,
+    totalSales: 398,
+  },
+  {
+    id: 3,
+    name: "BMW Extreme",
+    image:
+      "https://www.pngall.com/wp-content/uploads/2016/07/Car-Free-Download-PNG.png",
+    productType: "Sports Cars",
+    price: 170000,
+    rating: 4,
+    timeLeft: 21,
+    totalSales: 147,
+  },
+];
   export default productData;
